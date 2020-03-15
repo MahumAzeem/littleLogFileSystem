37757f7febe9c66869d1c09843aecda8a9f02af4
mahumazeem <azeem.mahum@gmail.com>
2020-03-15 14:45:00 -0700

    Added initLLFS method

---
+++ disk/disk.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/******Disk Params******/
+#define BLOCK_SIZE 512 //bytes
+#define NUM_BLOCKS 4096 //on Disk, numbered 0 to 4095
+#define VDISK "../disk/vdisk" //disk location
+#define MAGIC_NUMBER 42
+#define IONODES 128 //>>>what is this supposed to be?
+
+/******Prototypes******/
+void writeBlock(FILE* disk, int blockNumber, char* data);
+
+int main(){
+
+  FILE *disk;
+  disk = fopen(VDISK, "rb+"); //TODO: What mode to open in
+  char *init = calloc(BLOCK_SIZE*NUM_BLOCKS, 1);
+  fwrite(init, BLOCK_SIZE * NUM_BLOCKS, 1, disk);
+  free(init);
+  fclose(disk);
+
+  disk = fopen(VDISK, "rb+");
+
+  char* buffer;
+  buffer = (char *) malloc(BLOCK_SIZE);
+  int magic = MAGIC_NUMBER;
+  int blocks = NUM_BLOCKS;
+  int ionodes = IONODES;
+
+  memcpy(buffer, &magic, sizeof(magic));
+  memcpy(buffer + sizeof(int) * 1, &blocks, sizeof(int));
+  memcpy(buffer + sizeof(int) * 2, &ionodes sizeof(int));
+
+  writeBlock(disk, 0, buffer);
+  free(buffer);
+  fclose(disk);
+}
+
+void writeBlock(FILE* disk, int blockNumber, char* data){
+  fseek(disk, blockNumber * BLOCK_SIZE, SEEK_SET);
+  fwrite(data, BLOCK_SIZE, 1, disk);
+}
