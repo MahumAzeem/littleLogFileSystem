6faa96577e839d40c995730f7cb2b7a7658ab6a2
mahumazeem <azeem.mahum@gmail.com>
2020-04-06 18:09:36 -0700

    fixed bug

--- disk/disk.h
+++ disk/disk.h
@@ -1,6 +1,6 @@
 #ifndef DISK_H
 #define DISK_H
-bool writeBlock(int blockNumber, char *data, int offset, int data_size);
+_Bool writeBlock(int blockNumber, char *data, int offset, int data_size);
 void readBlock(FILE* disk, int blockNum, char* data);
 void InitLLFS();
 #endif
