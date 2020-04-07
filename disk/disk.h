0bd609dde9e0f35e914f6b25f907b79283cfc6df
mahumazeem <azeem.mahum@gmail.com>
2020-04-06 18:05:22 -0700

    added bool enum

--- disk/disk.h
+++ disk/disk.h
@@ -1,6 +1,10 @@
 #ifndef DISK_H
 #define DISK_H
-typedef enum {true, false} bool;
+typedef enum
+{
+    false = ( 1 == 0 ),
+    true = ( ! false )
+} bool;
 bool writeBlock(int blockNumber, char *data, int offset, int data_size);
 void readBlock(FILE* disk, int blockNum, char* data);
 void InitLLFS();
